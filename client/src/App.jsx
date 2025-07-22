b5124213856a9103f9bc62e12f17432f41f4ccd1
yashj-010 <yj99901@gmail.com>
2025-07-22 19:03:01 +0530

    server.js vercel connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import { AuthContext } from '../context/AuthContext'
 const App = () => {
   const { authUser } = useContext(AuthContext)
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain text-white">
+    <div className="bg-[url('/bgImage.svg')] bg-contain text-white">
       <Toaster/>
       <Routes>
         {/* <Route path='/' element={ <HomePage/> } />
--- server/server.js
+++ server/server.js
@@ -49,5 +49,14 @@ app.use("/api/messages",messageRouter)
 // Connect to Database
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT,()=>console.log("Server is running on PORT: " + PORT));
+
+if(process.env.NODE_ENV !== "production"){
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT,()=>console.log("Server is running on PORT: " + PORT));
+}else{
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT,()=>console.log("Server is running on PORT: " + PORT));
+}
+
+// Export server for versel
+export default server
