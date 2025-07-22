b0e7d6592409d900ff8d6110480718fc9dfc192c
yashj-010 <yj99901@gmail.com>
2025-07-22 19:07:50 +0530

    else wala part glt tha

--- server/server.js
+++ server/server.js
@@ -53,9 +53,6 @@ await connectDB();
 if(process.env.NODE_ENV !== "production"){
     const PORT = process.env.PORT || 5000;
     server.listen(PORT,()=>console.log("Server is running on PORT: " + PORT));
-}else{
-    const PORT = process.env.PORT || 5000;
-    server.listen(PORT,()=>console.log("Server is running on PORT: " + PORT));
 }
 
 // Export server for versel
